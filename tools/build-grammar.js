f021c31d34d0a52337a4d3c88b3d096971f8bbf2
idleberg <git@idleberg.com>
2019-02-19 22:06:09 +0100

    first commit

---
+++ tools/build-grammar.js
@@ -0,0 +1,35 @@
+// Dependencies
+const CSON = require('cson');
+const toCapitalCase = require('to-capital-case');
+const toSlugCase = require('to-slug-case');
+const { join } = require('path');
+const { languages, meta } = require('@nsis/language-data');
+const { writeFile } = require('fs');
+const languageNames = Object.keys(languages);
+
+languageNames.forEach(language => {
+    const long = (meta[language] && meta[language].long) ? meta[language].long : language;
+    const capital = toCapitalCase(long);
+    const slug = toSlugCase(long);
+
+    const output = {
+        'fileTypes': [
+            `${capital}.nlf`
+        ],
+        'name': `NSIS Language File (${capital})`,
+        'patterns': [
+            {
+                include: "source.nlf"
+            }
+        ],
+        "scopeName": `source.nlf.${slug}`
+    };
+
+    const outName = `nlf-${slug}.cson`;
+    const outFile = join(__dirname, '..', 'grammars', outName);
+    const contents = CSON.stringify(output, null, 2);
+
+    writeFile(outFile, contents, (err) => {
+      if (err) throw err;
+    });
+});
---
+++ tools/build-snippets.js
@@ -0,0 +1,39 @@
+// Dependencies
+const CSON = require('cson');
+const toCapitalCase = require('to-capital-case');
+const toSlugCase = require('to-slug-case');
+const { join } = require('path');
+const { languages, meta } = require('@nsis/language-data');
+const { writeFile } = require('fs');
+
+const languageNames = Object.keys(languages);
+const output = {};
+
+languageNames.forEach(language => {
+    const long = (meta[language] && meta[language].long) ? meta[language].long : language;
+    const capital = toCapitalCase(long);
+    const slug = toSlugCase(long);
+
+    const snippets = {};
+
+    Object.keys(languages[language].strings).forEach((stringKey, index ) => {
+        const key = stringKey;
+        const value = languages[language].strings[key];
+
+        const snippet = {
+            'body': `# ^${key}\n$\{1:${value}\}$0`,
+            'prefix': key,
+        };
+        snippets[key] = snippet;
+    })
+
+    output[`.source.nlf.${slug}`] = snippets;
+});
+
+const outName = 'core.NLF.cson';
+const outFile = join(__dirname, '..', 'snippets', outName);
+const contents = CSON.stringify(output, null, 2);
+
+writeFile(outFile, contents, (err) => {
+    if (err) throw err;
+});
